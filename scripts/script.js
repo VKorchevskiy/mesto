f1387912330e97cb377fe1ebf02bcf785ad26b2e
Vladimir Korchevskiy <burg2007@yandex.ru>
2020-12-21 23:27:09 +0300

    начать скрипт отправки формы

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+let editButton = document.querySelector('.botton_edit');
+let overlay = document.querySelector('.overlay');
+let closeButton = overlay.querySelector('.botton_close');
+
+let togglePopup = function() {
+  overlay.classList.toggle('overlay_active');
+}
+
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
+
+overlay.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+    togglePopup();
+  }
+});
+
+let formElement = overlay.querySelector('.form');
+
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+
+  let nameInput = document.querySelector('.input-text_type_name');
+  let jobInput = document.querySelector('.input-text_type_job');
+
+ /*  let nameInputValue = nameInput.getAttribute('value');
+  console.log(nameInputValue) */
+
+  // Получите значение полей из свойства value
+
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
