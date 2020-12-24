74566832e7d6dc8446d66b9882146420383db62d
Vladimir Korchevskiy <burg2007@yandex.ru>
2020-12-25 00:17:46 +0300

    refactor: исправить верстку header-а, добавить миксы для внешней геометрии

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,17 @@
 let editButton = document.querySelector('.button_edit');
 let overlay = document.querySelector('.overlay');
 let closeButton = overlay.querySelector('.button_close');
+let formElement = overlay.querySelector('.form');
+let nameInput = document.querySelector('.input-text_type_name');
+let jobInput = document.querySelector('.input-text_type_job');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
 
 let togglePopup = function () {
   overlay.classList.toggle('overlay_active');
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+
 
 overlay.addEventListener('click', (event) => {
   if (event.target === event.currentTarget) {
@@ -15,23 +19,17 @@ overlay.addEventListener('click', (event) => {
   }
 });
 
-let formElement = overlay.querySelector('.form');
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
 
-  let nameInput = document.querySelector('.input-text_type_name');
-  let jobInput = document.querySelector('.input-text_type_job');
 
-  let nameInputValue = nameInput.value;
-  let jobInputValue = jobInput.value;
 
-  let profileName = document.querySelector('.profile__name');
-  let profileDescription = document.querySelector('.profile__description');
-
-  profileName.textContent = nameInputValue;
-  profileDescription.textContent = jobInputValue;
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
   togglePopup();
 }
 
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup);
 formElement.addEventListener('submit', handleFormSubmit);
