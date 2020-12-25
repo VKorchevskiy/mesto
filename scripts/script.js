2eb70749083392add22c2e454f2c132c570f0998
Корчевский Владимир Владимирович <korchevsky@20cpi.org>
2020-12-25 14:53:52 +0300

    refactot: разделить методы открытия и закрытия popup-а

--- scripts/script.js
+++ scripts/script.js
@@ -7,29 +7,27 @@ let jobInput = document.querySelector('.form__input-text_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
 
-let togglePopup = function () {
-  overlay.classList.toggle('overlay_active');
+let openPopup = function () {
+  overlay.classList.add('overlay_active');
 }
 
-
-
-overlay.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-    togglePopup();
-  }
-});
-
+let closePopup = function () {
+  overlay.classList.remove('overlay_active');
+}
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-
 
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
   togglePopup();
 }
 
-editButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+overlay.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+});
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
