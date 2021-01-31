7c5382ae398288842f3d34fab5abbcc63b1dabd6
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-01-31 21:53:25 +0300

    feet: close popup escape and input error

--- scripts/script.js
+++ scripts/script.js
@@ -22,6 +22,67 @@ const closePopupImageButton = popupTypeImage.querySelector('.popup__close');
 const popupImage = popupTypeImage.querySelector('.popup__image');
 const popupCaption = popupTypeImage.querySelector('.popup__caption');
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('form__input-text_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('form__input-text_type_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input-text'));
+  const buttonElement = formElement.querySelector('.form__save');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('form__save_inactive');
+  }
+  else {
+    buttonElement.classList.remove('form__save_inactive');
+  }
+}
+
+enableValidation();
+
 function createCard(el) {
   const card = cardTemplate.cloneNode(true);
   const imageButton = card.querySelector('.card__image');
@@ -95,12 +156,19 @@ function handleCloseOverlay(event) {
   }
 }
 
+function handleCloseEscOverlay(event) {
+  if (event.key === 'Escape' && document.querySelector('.popup_active')) {
+    handleClosePopup(document.querySelector('.popup_active'));
+  }
+}
+
 function handleCardConteiner(event) {
   if (event.target.classList.contains('card__like')) likeCard(event);
   if (event.target.classList.contains('card__trash')) deleteCard(event);
   if (event.target.classList.contains('card__image')) initImagePopup(event);
 }
 
+document.addEventListener('keydown', handleCloseEscOverlay);
 document.addEventListener('click', handleCloseOverlay);
 cardContainer.addEventListener('click', handleCardConteiner);
 editButton.addEventListener('click', handleInitProfilePopup);
