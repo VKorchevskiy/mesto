e5818a93ccd7bbef2254333218702b386107cb78
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-03-08 21:14:48 +0300

    refactor: изменить структуру

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -14,7 +14,6 @@ export default class PopupWithForm extends Popup {
   _getInputValues() {
     this._formValues = {};
     this._inputList.forEach(input => this._formValues[input.name] = input.value);
-    console.log(this._formValues)
     return this._formValues;
   }
 
